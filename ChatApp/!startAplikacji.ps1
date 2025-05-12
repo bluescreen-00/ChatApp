ecbbf743f2feeeb98a35e27a2d7c0d01dbe14334
AW_LS_RS_PZSI <adamww202@gmail.com>
2025-05-12 15:27:39 +0200

    Delete ChatApp directory

--- ChatApp/!startAplikacji.ps1
+++
@@ -1,7 +0,0 @@
-# Uruchom backend w nowym oknie PowerShell
-
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd $PWD/backend; npm run server" -WindowStyle Normal
-
-# Uruchom frontend w nowym oknie PowerShell
-
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd $PWD/frontend; npm run dev" -WindowStyle Normal
